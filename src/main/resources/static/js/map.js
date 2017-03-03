c65b3a6dd4b8cc3aa841ee2fd4ad097d700477f3
Travis Caro <travisbcaro@gmail.com>
2017-03-02 23:31:45 -0500

    Begin setting up prototype structure

---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  var appVars = { };
+  var app = new App();
+  app.initialize();
+});
+
+function App(options) {
+  var self = this;
+
+  var elements = { };
+
+  self.initialize = function() {
+
+  };
+
+  var bindEventHandlers = function() {
+
+  };
+}
