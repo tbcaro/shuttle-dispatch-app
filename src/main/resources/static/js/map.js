b711a3e2c590cc56fb5495ad0313b296c1966e67
Travis Caro <travisbcaro@gmail.com>
2017-03-04 17:04:56 -0500

    Add google map integration

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,16 +1,33 @@
-$(document).ready(function(){
-  var appVars = { };
-  var app = new App();
-  app.initialize();
-});
-
 function App(options) {
   var self = this;
 
   var elements = { };
 
-  self.initialize = function() {
+  self.geoLocator = { };
+  self.googleMapsLoader = { };
+  self.map = { };
 
+  self.initialize = function() {
+    // TBC : Setup elements
+    elements.mapContainer = $('#map-container');
+    elements.activeShuttleCardContainer = $('#active-shuttle-card-container');
+
+    // TBC : Setup utility objects
+    self.geoLocator = new GeoLocator();
+    
+    self.geoLocator.getLocation().done(function (location) {
+      self.initializeMap(10, location);
+    });
+  };
+
+  self.initializeMap = function(_zoom, _center) {
+    var zoom = _zoom || 10;
+    var center = _center || { lat: 100, lng: 100 };
+
+    self.map = new google.maps.Map(elements.mapContainer[0], {
+      center: center,
+      zoom: zoom
+    });
   };
 
   var bindEventHandlers = function() {
