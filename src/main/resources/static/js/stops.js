be66c649fbf3315d6c8426b0ee47238026c5e8d1
Travis Caro <travisbcaro@gmail.com>
2017-03-10 14:04:55 -0500

    Setup base layout and styles for stops page

---
+++ src/main/resources/static/js/stops.js
@@ -0,0 +1,52 @@
+
+function StopApp(options) {
+  var self = this;
+
+  var elements = { };
+
+  var geoLocator = { };
+
+  self.map = { };
+  self.mapMarkers = { };
+
+  self.initialize = function() {
+    // TBC : Setup elements
+    elements.mapContainer = $('#map-container');
+
+    // TBC : Setup utility objects
+    geoLocator = new GeoLocator();
+    self.initializeMap();
+    geoLocator.getLocation().done(function(location) {
+      self.map.setCenter(location);
+      self.map.setZoom(12);
+      self.mapMarkers.stopMarker = new google.maps.Marker({
+        position: location,
+        map: self.map,
+        icon: {
+          path: fontawesome.markers.MAP_PIN,
+          scale: 0.5,
+          strokeWeight: 0.2,
+          strokeColor: 'black',
+          strokeOpacity: 1,
+          fillColor: '#ff0000',
+          fillOpacity: 1,
+          rotation: 0
+        }
+      });
+    });
+  };
+
+  self.initializeMap = function() {
+    self.map = new google.maps.Map(elements.mapContainer[0], {
+      center: { lat: 39.8282, lng: -98.5795 }, // TBC : Default center over central USA
+      zoom: 5
+    });
+  };
+
+  var bindEventHandlers = function() {
+
+  };
+
+  self.initialize();
+  return self;
+}
