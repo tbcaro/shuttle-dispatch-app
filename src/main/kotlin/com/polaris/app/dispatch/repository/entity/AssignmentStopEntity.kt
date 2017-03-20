c5c58f332124188e225d8af9c5ca7a59d324ac7d
Tyler Holben <tsholben@gmail.com>
2017-03-20 16:20:30 -0400

    Edit Services and Repos

--- src/main/kotlin/com/polaris/app/dispatch/repository/entity/AssignmentStopEntity.kt
+++ src/main/kotlin/com/polaris/app/dispatch/repository/entity/AssignmentStopEntity.kt
@@ -1,12 +1,13 @@
 package com.polaris.app.dispatch.repository.entity
 
+import java.math.BigDecimal
 import java.time.LocalTime
 
 data class AssignmentStopEntity(
         val stopName: String,
         val stopAddress: String,
-        val stopLat: String,
-        val stopLong: String,
+        val stopLat: BigDecimal,
+        val stopLong: BigDecimal,
         val stopArrive: LocalTime,
         val stopDepart: LocalTime,
         val stopArriveEst: LocalTime,
--- src/main/kotlin/com/polaris/app/dispatch/service/bo/MapAssignmentStop.kt
+++ src/main/kotlin/com/polaris/app/dispatch/service/bo/MapAssignmentStop.kt
@@ -1,12 +1,13 @@
 package com.polaris.app.dispatch.service.bo
 
+import java.math.BigDecimal
 import java.time.LocalTime
 
 data class MapAssignmentStop(
         val stopName: String,
         val stopAddress: String,
-        val stopLat: String,
-        val stopLong: String,
+        val stopLat: BigDecimal,
+        val stopLong: BigDecimal,
         val stopArrive: LocalTime,
         val stopDepart: LocalTime,
         val stopArriveEst: LocalTime,
