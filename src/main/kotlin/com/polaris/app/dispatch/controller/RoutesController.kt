1c435cb52865202b6a9ab4f37734bd39a4efc081
Travis Caro <travisbcaro@gmail.com>
2017-04-11 21:22:28 -0400

    Setup controllers for routes and stops

--- src/main/kotlin/com/polaris/app/dispatch/controller/RoutesController.kt
+++ src/main/kotlin/com/polaris/app/dispatch/controller/RoutesController.kt
@@ -1,18 +1,25 @@
 package com.polaris.app.dispatch.controller
 
+import com.polaris.app.dispatch.controller.exception.AuthenticationException
+import com.polaris.app.dispatch.service.AuthenticationService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import javax.servlet.http.HttpServletRequest
 
 @Controller
-class RoutesController {
+class RoutesController(private val authService: AuthenticationService) {
 
     @RequestMapping("/routes")
-    fun routes(model: Model) : String {
+    fun routes(model: Model, http: HttpServletRequest) : String {
+        if (authService.isAuthenticated(http)) {
+            val userContext = authService.getUserContext(http)
+            model.addAttribute("title", "Routes")
+            model.addAttribute("username", userContext.username)
 
-        model.addAttribute("title", "Routes")
-        model.addAttribute("username", "tcaro")
-
-        return "routes"
+            return "routes"
+        } else {
+            throw AuthenticationException()
+        }
     }
 }
--- src/main/kotlin/com/polaris/app/dispatch/controller/StopsController.kt
+++ src/main/kotlin/com/polaris/app/dispatch/controller/StopsController.kt
@@ -1,18 +1,25 @@
 package com.polaris.app.dispatch.controller
 
+import com.polaris.app.dispatch.controller.exception.AuthenticationException
+import com.polaris.app.dispatch.service.AuthenticationService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import javax.servlet.http.HttpServletRequest
 
 @Controller
-class StopsController {
+class StopsController(private val authService: AuthenticationService) {
 
-    @RequestMapping("/assignmentStops")
-    fun stops(model: Model) : String {
+    @RequestMapping("/stops")
+    fun stops(model: Model, http: HttpServletRequest) : String {
+        if (authService.isAuthenticated(http)) {
+            val userContext = authService.getUserContext(http)
+            model.addAttribute("title", "Stops")
+            model.addAttribute("username", userContext.username)
 
-        model.addAttribute("title", "Stops")
-        model.addAttribute("username", "tcaro")
-
-        return "assignmentStops"
+            return "stops"
+        } else {
+            throw AuthenticationException()
+        }
     }
 }
