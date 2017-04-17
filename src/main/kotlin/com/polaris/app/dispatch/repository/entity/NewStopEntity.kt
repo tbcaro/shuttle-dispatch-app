58b08101f094f70bd6edf2c495721f295750609e
Tyler Holben <tsholben@gmail.com>
2017-04-17 14:20:40 -0400

    Attempt implementation of Assignment update and Stop/Route pages

---
+++ src/main/kotlin/com/polaris/app/dispatch/repository/entity/NewStopEntity.kt
@@ -0,0 +1,5 @@
+package com.polaris.app.dispatch.repository.entity
+
+/**
+ * Created by Tyler on 4/16/2017.
+ */
---
+++ src/main/kotlin/com/polaris/app/dispatch/service/bo/NewStop.kt
@@ -0,0 +1,5 @@
+package com.polaris.app.dispatch.service.bo
+
+/**
+ * Created by Tyler on 4/16/2017.
+ */
